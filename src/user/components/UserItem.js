f32beba1e4e8426d4dd4cb79c2bd91708f0ccb99
Garosan <garosanchez27@gmail.com>
2021-06-01 18:41:44 -0500

    Setup UsersList component and create dummy array of users in Users page

---
+++ src/user/components/UserItem.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./UsersItem.css";
+
+const UsersItem = () => {
+  return <h2>UsersItem component</h2>;
+};
+
+export default UsersItem;
--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,32 @@
 import React from "react";
 
+import UsersList from "../components/UsersList";
+
 const Users = () => {
-  return <h2>Users component</h2>;
+  const DUMMY_USERS = [
+    {
+      id: "u1",
+      image:
+        "https://cencup.com/wp-content/uploads/2019/07/avatar-placeholder.png",
+      name: "Garosan",
+      places: 3,
+    },
+    {
+      id: "u2",
+      image:
+        "https://cencup.com/wp-content/uploads/2019/07/avatar-placeholder.png",
+      name: "Maximilian",
+      places: 5,
+    },
+    {
+      id: "u3",
+      image:
+        "https://cencup.com/wp-content/uploads/2019/07/avatar-placeholder.png",
+      name: "Manu",
+      places: 2,
+    },
+  ];
+  return <UsersList items={DUMMY_USERS} />;
 };
 
 export default Users;
