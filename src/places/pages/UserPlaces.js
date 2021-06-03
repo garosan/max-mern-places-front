ec25c982a56866c6c535439f87f2161a502f47ff
Garosan <garosanchez27@gmail.com>
2021-06-02 21:28:34 -0500

    Add UserPlaces page, PlaceItem and PlaceList component and useParams from react-router-dom to filter places to show from DUMMY_PLACES array

---
+++ src/places/pages/UserPlaces.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import PlaceList from "../components/PlaceList";
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world!",
+    imageUrl:
+      "https://s1.eestatic.com/2019/06/27/actualidad/actualidad_409472266_130589124_1024x576.jpg",
+    address: "20 W 34th St, New York, NY, 10001",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584,
+    },
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world!",
+    imageUrl:
+      "https://s1.eestatic.com/2019/06/27/actualidad/actualidad_409472266_130589124_1024x576.jpg",
+    address: "20 W 34th St, New York, NY, 10001",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584,
+    },
+    creator: "u2",
+  },
+];
+
+const UserPlaces = () => {
+  const userId = useParams().userId;
+  const loadedPlaces = DUMMY_PLACES.filter(
+    (places) => places.creator === userId
+  );
+  return <PlaceList items={loadedPlaces} />;
+};
+
+export default UserPlaces;
